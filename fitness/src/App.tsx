2c6b0bed54f3879130255508e2d70584c8884e07
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-14 13:23:09 -0500

    Adding context API and global context. Components as well

--- fitness/src/App.tsx
+++ fitness/src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { Header } from "../components/Header";
+import { Header } from "../src/components/Header";
+import { Balance } from "../src/components/Balance";
+import "./App.css";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
-    </div>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
